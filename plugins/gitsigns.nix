768620ec21a2475158f48b807ee68efca9d65b50
Hallam Smith <me@hcssmith.com>
2024-06-27 12:14:42 +0100

    feat(git): open current blamme commit

--- plugins/gitsigns.nix
+++ plugins/gitsigns.nix
@@ -34,5 +34,33 @@
       key = "[c";
       mode = ["n"];
     }
+    {
+      action = ''
+          function()
+        local gitsigns = require('gitsigns')
+              -- Open popup window with commit of blamed line.
+              gitsigns.blame_line(
+                  { full = false },
+                  function()
+                      -- In order to focus opened popup window, blame_line needs to be called again.
+                      gitsigns.blame_line(
+                          {},
+                          function()
+                              -- Now that popup is focused, extract commit SHA from the start of it.
+                              local blamed_commit = vim.fn.getline(1):match("^(%x+)")
+
+                              -- Close the focused popup.
+                              vim.cmd(":quit")
+
+              				require("neogit.buffers.commit_view").new(blamed_commit):open()
+                              -- Rest of the code that has to be executed synchronously after blamed commit is extracted.
+                          end)
+                  end)
+          		end
+      '';
+      lua = true;
+      mode = ["n"];
+      key = "<leader>gb";
+    }
   ];
 }
